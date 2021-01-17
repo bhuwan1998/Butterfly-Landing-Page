18dac4fbbdd045e295ee7b66c20a607e86ebcd05
bhuwan1998 <rfperfect785@gmail.com>
2021-01-17 21:14:19 +1100

    Fixed About section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,16 +7,18 @@ export class about extends Component {
         <div id="about">
         <div className="container">
           <div className="row">
-            <div className="col-xs-12 col-md-6"> <img src="img/about.jpg" className="img-responsive" alt=""/> </div>
-            <div className="col-xs-12 col-md-6">
+            <div className="col-xs-12 col-md-8">
               <div className="about-text">
+                
+                <div className="col-md col-xs-12">
                 <h2>About Us</h2>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
                 <p>{this.props.data ? this.props.data.paragraph : 'loading...'}</p>
   
                 </div>
               </div>
             </div>
+            <div className="col-xs-12 col-md-4"> <img src="img/about.jpg" className="img-responsive" alt=""/> </div>
+
           </div>
         </div>
       </div>
